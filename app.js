95df8de8cf06a46fffa7c74cd3bd7854b90e8aa8
unknown <mkholowthandow@gmail.com>
2023-01-22 04:30:03 +0200

    commit 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-const bodyParser = require("body-parser");
-const flash = require("connect-flash");
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
 const path = require('path');
 const session = require('express-session');
 const ejsMate = require('ejs-mate');
@@ -10,32 +10,30 @@ const app = express();
 
 const userRoutes = require('./routes/userRoutes');
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public/'));
 app.use(flash());
 
-app.use(require('express-session')({
-    secret: 'this is my fifth fullstack',
-    resave: false,
-    saveUninitialized: false
-}));
-
+app.use(
+    require('express-session')({
+        secret: 'this is my fifth fullstack',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
 app.use((req, res, next) => {
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
     res.locals.currentUser = req.user;
     next();
 });
 
+app.use('/', userRoutes);
 
-
-app.use("/", userRoutes);
-
-app.listen('70', () => {
-    console.log('Connected to port 70')
-})
+app.listen(process.env.Port, () => {
+    console.log('Connected to port 70');
+});
